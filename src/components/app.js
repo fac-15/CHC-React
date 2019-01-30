0ab6df01ae8bd819954492d6b00766169349710e
Hannah <hannahstewart23@yahoo.co.uk>
2019-01-30 16:45:25 +0000

    some branch logic but mostly working counter

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,28 @@
 import React from 'react';
-
+import LeafComp from './leafComp';
+// import PlantPotComp from './plantPotComp';
 export default class App extends React.Component {
   state = {
-    flower: 'Hello, i\'m a flower'
+    stalk: 0,
+    leaf: 222,
+    flower: 0,
+    count: 0,
+    timer: 0,
+    timerId: 0
   }
 
   render() {
     return (
       <div>
-        <p>{this.props.flower}</p>
+        <LeafComp />
+        <p> leaf {this.state.leaf}</p>
+        <p> count {this.state.count}</p>
+        <button
+          onClick= {() =>
+            this.setState(prevState => {
+              return { count: prevState.count += 1 }})
+          }
+        > click </button>
       </div>
     )
   }
---
+++ src/components/plantPotComp.js
@@ -0,0 +1,15 @@
+
+// class PlantPotComp {
+//   constructor(){
+//     this.stalk = 0;
+//     this.leaf = 0;
+//     this.flower = 0;
+//   }
+// }
+//
+//
+//
+//
+//
+//
+// export default PlantPotComp;
