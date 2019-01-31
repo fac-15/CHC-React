1236dcd80e77d5c446626a8cefea8a323f498080
Charlie La Fosse <charlielafosse@outlook.com>
2019-01-31 12:47:27 +0000

    point stealing functionality added to app component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import StalkComp from './stalkComp';
-import LeafComp from './leafComp';
-import FlowerComp from './flowerComp';
-import PlantPotComp from './plantPotComp';
-import CloudComp from './cloudComp';
+import React from "react";
+import StalkComp from "./stalkComp";
+import LeafComp from "./leafComp";
+import FlowerComp from "./flowerComp";
+import PlantPotComp from "./plantPotComp";
+import CloudComp from "./cloudComp";
 
 export default class App extends React.Component {
   state = {
@@ -13,41 +13,50 @@ export default class App extends React.Component {
     totalCount: 0,
     timer: 0,
     timerId: 0,
+    opponentTimerId: 0,
     active: false
-  }
+  };
 
   start = () => {
-
     const timer = () => {
-      this.setState((prevState) => {
-        return { timer: prevState.timer +=1 }
-      })
-    }
+      this.setState(prevState => {
+        return { timer: (prevState.timer += 1) };
+      });
+    };
 
-    if(!this.state.active) {
+    const opponentTimer = () => {
+      this.setState(prevState => {
+        return { totalCount: (prevState.totalCount -= 3) };
+      });
+    };
+
+    if (!this.state.active) {
       this.setState(() => {
-        const id = setInterval(timer, 1000)
-        return { active: true, timerId: id };
-      })
+        const id = setInterval(timer, 1000);
+        const oppId = setInterval(opponentTimer, 4000);
+        return { active: true, timerId: id, opponentTimerId: oppId };
+      });
     }
-  }
-
-
+  };
+  pointCounter = () => {
+    this.setState({ totalCount: this.state.totalCount + 1 });
+  };
 
   render() {
     return (
       <div>
-      <p className="timer"> timer: {this.state.timer}</p>
-      <button onClick={this.start}>Begin</button>
-      <div className="container">
-        <CloudComp/>
-        <FlowerComp />
-        <LeafComp />
-        <StalkComp />
-        <PlantPotComp />
+        <p className="timer"> timer: {this.state.timer}</p>
+        <p className="timer"> points: {this.state.totalCount}</p>
+        <button onClick={this.start}>Begin</button>
+        <button onClick={this.pointCounter}>Water Me!</button>
+        <div className="container">
+          <CloudComp />
+          <FlowerComp />
+          <LeafComp />
+          <StalkComp />
+          <PlantPotComp />
+        </div>
       </div>
-      </div>
-
     );
   }
 }
